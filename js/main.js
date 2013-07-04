1837195b5268c76b42700967b303d3ee79cef224
Krasu <krasniyrus@gmail.com>
2013-07-04 13:37:55 +0400

    draft

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
 $(function () {
-    var enableImpress = true
+    var enableImpress = true,
+        impressEl = $('#impress'),
+        frame = $('.frame'),
+        stepTpl = $('#step-')
 
     for (var i = 0; i <= 8; i++) {
-        $('#step-').clone().attr('id', 'step-' + i).appendTo('#impress')
+        stepTpl.clone().attr('id', 'step-' + i).appendTo(impressEl)
     }
-    for (var i = 0; i < 7; i++) {
-        $('#cutout-').clone().attr('id', '').appendTo($('#overview .wheel'))
-    }
+    stepTpl.remove()
 
-    $('#step-, #cutout-').remove()
-
-    var elems = $('.step.img'),
+    var images = $('.step.img'),
         radius = 2000;
 
-    elems.each(function (i, elem) {
-        var theta = -i / (elems.length - 1) * 2 * Math.PI,
+    images.each(function (i, elem) {
+        var theta = -i / (images.length - 1) * 2 * Math.PI,
             x = Math.round(radius * Math.cos(theta)),
             y = Math.round(radius * Math.sin(theta)),
             rotation = Math.round(theta / (2 * Math.PI) * 360 - 90);
@@ -26,34 +25,35 @@ $(function () {
         })
     })
 
-    elems = $('.cutout'),
-        radius = 450;
-console.log($('.cutout'))
-    var increase = Math.PI * 2 / elems.length;
-    var angle = 0;
-
-    elems.each(function (i, elem) {
-        var theta = i / (elems.length - 1) * 2 * Math.PI,
-            x = radius * Math.cos(angle) + radius,
-            y = radius * Math.sin(angle) + radius,
-            rotation = Math.round(theta / (2 * Math.PI) * 360 - 90);
-
-        console.log(theta)
-        $(elem).css({
-            'top': x,
-            'left': y,
-//            '-webkit-transform': 'rotate(' + rotation + 'deg)'
-        })
-
-        angle += increase;
-    })
-
-
     $('#overview').attr({
         'data-x': 0,
         'data-y': 0,
         'data-scale': 5
     })
 
+    function replaceFrame() {
+        var computedStyle = window.getComputedStyle(impressEl[0], null),
+            matrix = computedStyle.getPropertyValue('transform')
+                || computedStyle.getPropertyValue('-moz-transform')
+                || computedStyle.getPropertyValue('-webkit-transform')
+                || computedStyle.getPropertyValue('-ms-transform')
+                || computedStyle.getPropertyValue('-o-transform'),
+            data = [1];
+
+        if (matrix != 'none') data = matrix.split('(')[1].split(')')[0].split(',');
+
+        frame.css({
+            width: 1100 * data[0],
+            height: 700 * data[0]
+        })
+
+        frame.css({
+            top: Math.max(0, (($(window).height() - frame.outerHeight()) / 2)),
+            left: Math.max(0, (($(window).width() - frame.outerWidth()) / 2))
+        })
+    }
+
     enableImpress && impress().init();
+    replaceFrame()
+    $(window).on('resize', replaceFrame);
 })
